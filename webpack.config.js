ed09e672b388f782bcc8b85534ff3c81eb50ebc2
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-08-26 12:44:02 +0400

    Chore add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "boundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+};
